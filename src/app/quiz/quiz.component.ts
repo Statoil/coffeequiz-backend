ae9c50d2923acc5fb1947cdeaf54618bd7d90132
oyron <oyron@statoil.com>
2018-01-01 22:16:38 +0100

    Use date on quiz object within quizItems

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -5,6 +5,7 @@ import {Quiz} from "../quiz";
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {QuizItem} from "../quizitem";
 import {QuizMetadataComponent} from "../quiz-metadata/quiz-metadata.component";
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-quiz',
@@ -15,6 +16,7 @@ export class QuizComponent implements OnInit {
 
     quiz: Quiz;
     quizItem: QuizItem;
+    private startWeekDay: number;
 
     constructor(
         private quizService: QuizService,
@@ -30,7 +32,10 @@ export class QuizComponent implements OnInit {
         const id = this.activatedRoute.snapshot.paramMap.get('id');
         console.log("id: " + id);
         this.quizService.getQuiz(id)
-            .then (quiz => this.quiz = quiz);
+            .then (quiz => {
+                this.quiz = quiz;
+                this.startWeekDay = QuizComponent.getWeekDay(quiz.startTime);
+            });
     }
 
     openQuizItem(quizItemId) {
@@ -51,5 +56,14 @@ export class QuizComponent implements OnInit {
     editMetadata() {
         const modalRef = this.modalService.open(QuizMetadataComponent);
         modalRef.componentInstance.quiz = this.quiz;
+    }
+
+    getQuizItemDate(index) {
+        const numberOfWeekEndsInRange = Math.floor((this.startWeekDay + index) / 5);
+        return moment(this.quiz.startTime).add(index + (numberOfWeekEndsInRange * 2), 'days').toDate();
+    }
+
+    static getWeekDay(date: Date): number {
+        return (moment(date).day() + 6) % 7;
     }
 }
