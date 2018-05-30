ea0ede63ccb85dcd0716e90ead8b035051051fac
oyron <oyron@statoil.com>
2018-05-30 10:58:32 +0200

    Allow for sending internet-explorer.html

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ app.use((req, res, next) => {
 
 app.use('/api', api);
 
+app.get('/internet-explorer.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/internet-explorer.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
