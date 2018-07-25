bc804042a3dc10dc2c3a7b6fce8ffac969cd448f
oyron <oyron@statoil.com>
2018-07-25 12:05:29 +0200

    Upgrade to Angular 6

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
+import {Observable} from "rxjs";
 import {Quiz} from "./quiz";
 import {QuizMetadata} from "./quizmetadata";
 import {catchError} from "rxjs/operators";
