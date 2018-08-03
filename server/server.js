99076a554907506274388dc3df1c5d4fba99a91b
oyron <oyron@statoil.com>
2018-08-03 15:01:51 +0200

    Added Terms of Service

--- server/server.js
+++ server/server.js
@@ -34,6 +34,10 @@ app.get('/ie', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/internet-explorer.html'));
 });
 
+app.get('/terms', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/terms.html'));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
