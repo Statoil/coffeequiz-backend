8633e070570bd321b69821b0dde96ac290cd195c
oyron <oyron@statoil.com>
2018-08-07 11:44:37 +0200

    orderBy in stats page

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -87,7 +87,7 @@ export class StatisticsComponent implements OnInit {
             statsPrQuestion.push(accumulatedStats);
         });
 
-        return _.sortBy(statsPrQuestion, (stat) => stat.quizItem.date.getTime());
+        return _.orderBy(statsPrQuestion, (stat) => stat.quizItem.date.getTime(), ['asc']);
     }
 
     private static getChartColors(answer: number):any {
